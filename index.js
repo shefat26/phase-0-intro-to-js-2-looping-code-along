8ce67ba0ffa15b9513bd282ce2decc97a43dcd8c
shefat26 <shefatmoral@gmail.com>
2024-12-18 12:02:04 -0500

    looping prep

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Code your solutions in this file
+
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+
+
+
+function writeCards(names, event){
+
+    const arr = []
+   
+    for(let i = 0; i<names.length; i++){
+
+        let msg = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+
+        arr.push(msg)
+    }
+   
+    return arr;
+
+}
+
+
+function countDown(count){
+
+    while(count>=0){
+
+        console.log(count )
+
+        count--
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
